2c2f7f612ee0e7477e56441e8cdbbbd17cc128c5
Garv Gupta <catocloud4357@gmail.com>
2025-10-07 15:03:57 +0530

    voice

--- voice_microservice.py
+++ voice_microservice.py
@@ -41,5 +41,5 @@ def home():
     return "Voice Microservice Active ✅"
 
 if __name__ == '__main__':
-    port = int(os.environ.get("PORT", 7860))
+    port = int(os.environ.get("PORT", 5000))
     app.run(host='0.0.0.0', port=port)
